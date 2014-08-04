dd22c022027470c32a82679e5bbb54aef15cf5e0
Brock Stone <BrockStone@fullsail.edu>
2014-08-03 21:29:04 -0400

    added WCF site for testing

---
+++ WCF/js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var wcf = angular.module('wcf', [
+	'ngRoute'
+])
+	.config(function($routeProvider) {
+		$routeProvider
+			//Main View 
+			.when('/', {
+				templateUrl: 'views/welcome.html'
+			})
+			// Contact View  
+			.when('/contact', {
+				templateUrl: 'views/contact.html'
+				// controller: 'contactCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
