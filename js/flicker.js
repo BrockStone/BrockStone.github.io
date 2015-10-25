570fadc5fcd52e0f65652f8ccc4ccb37494387a3
Brock Stone <brockstonedesign@gmail.com>
2015-10-25 14:53:47 -0400

    updated main navigation styling

--- js/flicker.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-     setInterval(function() {
-            var val = 1;
-            if (Math.random() > 0.5) {
-                val = Math.floor((Math.random()*10)+1);
-            }   
-            $(".flickr").css("text-shadow", "white 0 0 " + val + "px");
-     }, 200);
-});
